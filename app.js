c743bb82856335837fce56acf945a0875cfe19ae
Daniel Olea <oleaonize@gmail.com>
2023-02-07 20:06:08 +0100

    Passport Auth casi completo

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ require("dotenv").config()
 
 const express = require("express")
 const logger = require("morgan")
-//const passport = require('passport');
+const passport = require('passport');
 const createError = require("http-errors")
-//const { sessionConfig } = require('./config/session.config')
+const { sessionConfig } = require('./config/session.config')
 const router = require('./config/routes.config');
 
 require("./config/db.config")
 require("./config/hbs.config")
-require("./config/routes.config")
+require('./config/passport.config');
 
 const app = express()
 
@@ -22,23 +22,37 @@ app.set("view engine", "hbs")
 
 app.use(express.static("public"));
 
+// Session middleware
+app.use(sessionConfig);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+})
+
 /** Router **/
 app.use('/', router)
 
+/**
+ * Error Middlewares
+ */
+
 
 app.use((req, res, next) => {
-	next(createError(404, "Resource not found"))
-})
-
-app.use((error, req, res, next) => {
+	next(createError(404, 'Resource not found'));
+  });
+  
+  app.use((error, req, res, next) => {
 	console.log(error)
-	let status = error.status || 500
-
-	res.status(status).render("error", {
-		message: error.message,
-		error: req.app.get("env") === "development" ? error : {},
+	let status = error.status || 500;
+  
+	res.status(status).render('error', {
+	  message: error.message,
+	  error: req.app.get('env') === 'development' ? error : {}
 	})
-})
+  })
 
 const PORT = process.env.PORT || 3000
 
---
+++ controllers/auth.controller.js
@@ -0,0 +1,85 @@
+const createError = require("http-errors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const User = require("../models/User.model");
+
+module.exports.register = (req, res, next) => {
+  res.render("auth/signup");
+};
+
+module.exports.doRegister = (req, res, next) => {
+  const renderWithErrors = (errors) => {
+    res.render("auth/signup", {
+      user: {
+        email: req.body.email,
+        username: req.body.username,
+      },
+      errors,
+    });
+  };
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return User.create(req.body).then((user) => {
+          res.redirect("/login");
+        });
+      } else {
+        renderWithErrors({ email: "Email already in use" });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        renderWithErrors(err.errors);
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  res.render("auth/login");
+};
+
+const doLoginWithStrategy = (req, res, next, strategy = 'local-auth') => {
+    if (strategy === "local-auth") {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        res.status(404).render('auth/login', { errorMessage: 'Email or password are incorrect' })
+      }
+    }
+  
+  
+  passport.authenticate(strategy, (err, user, validations) => {
+    if (err) {
+      next(err)
+    } else if (!user) {
+      console.log({ errorMessage: validations.error })
+      res.status(404).render('auth/login', { user: { email: email }, errorMessage: validations.error })
+    } else {
+      req.login(user, (loginError) => {  
+        if (loginError) {
+          next(loginError)
+        } else {
+          res.redirect('/profile')
+        }
+      })
+    }
+  })(req, res, next)
+  }
+  
+  
+  module.exports.doLogin = (req, res, next) => {
+    doLoginWithStrategy(req, res, next)
+  }
+
+  module.exports.doLoginGoogle = (req, res, next) => {
+    doLoginWithStrategy(req, res, next, 'google-auth')
+  }
+
+
+
+module.exports.profile = (req, res, next) => {
+    res.render("user/profile")
+  }
